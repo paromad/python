86e01b3203b218611637efc1f764e61fbaf3f513
paromad <luba100901@mail.ru>
2020-04-23 20:28:40 +0300

    like normal code

--- encryptor.py
+++ encryptor.py
@@ -3,6 +3,7 @@ import string
 import argparse
 import sys
 
+
 lowercase = string.ascii_lowercase
 uppercase = lowercase.upper()
 
@@ -24,11 +25,9 @@ def encryption_caesar(s, key):
 
 def encryption_vigenere_symbol(i, num, key):
     if i in lowercase:
-        return lowercase[(lowercase.index(i) +
-                          lowercase.index(key[num % len(key)].lower())) % 26]
+        return lowercase[(lowercase.index(i) + lowercase.index(key[num % len(key)].lower())) % 26]
     if i in uppercase:
-        return uppercase[(uppercase.index(i) +
-                          lowercase.index(key[num % len(key)].lower())) % 26]
+        return uppercase[(uppercase.index(i) + lowercase.index(key[num % len(key)].lower())) % 26]
     return i
 
 
@@ -59,11 +58,13 @@ def decryption_caesar(s, key):
 
 def decryption_vigenere_symbol(i, num, key):
     if i in lowercase:
-        return lowercase[(lowercase.index(i) + 26 -
-                          lowercase.index(key[num % len(key)].lower())) % 26]
+        return lowercase[
+            (lowercase.index(i) + 26 - lowercase.index(key[num % len(key)].lower())) % 26
+        ]
     if i in uppercase:
-        return uppercase[(uppercase.index(i) + 26 -
-                          lowercase.index(key[num % len(key)].lower())) % 26]
+        return uppercase[
+            (uppercase.index(i) + 26 - lowercase.index(key[num % len(key)].lower())) % 26
+        ]
     return i
 
 
@@ -92,13 +93,13 @@ def bar_chart(s):
 
 
 def hack(s, dict):
-    diff = 26 * 26
+    diff = None
     key = 0
     for i in range(26):
         res = encryption_caesar(s, i)
         arr = bar_chart(res)
         comp = compare_chart(dict, arr)
-        if comp < diff:
+        if diff is None or comp < diff:
             diff = comp
             key = i
     return key
@@ -111,58 +112,107 @@ def compare_chart(dict, arr):
     return diff
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('act')
-parser.add_argument('--cipher')
-parser.add_argument('--key', dest='key')
-parser.add_argument('--input-file', dest='input_file')
-parser.add_argument('--output-file', dest='output_file')
-parser.add_argument('--text-file', dest='text_file')
-parser.add_argument('--model-file', dest='model_file')
-args = parser.parse_args()
-
-key = args.key
-if args.cipher == "caesar":
-    key = int(args.key)
-
-if args.input_file is not None:
-    with open(args.input_file, 'r') as f:
-        s = f.read()
-else:
-    if args.act != "train":
+def get_text(args):
+    if args.input_file is not None:
+        with open(args.input_file, "r") as f:
+            s = f.read()
+    else:
         s = sys.stdin.read()
+    return s
 
-if args.act == "encode":
+
+def write_text(args, res):
+    if args.output_file is not None:
+        with open(args.output_file, "w") as f:
+            f.write(res)
+    else:
+        print(res)
+
+
+def process_encode(args):
+    key = args.key
+    if args.cipher == "caesar":
+        key = int(args.key)
+
+    s = get_text(args)
+
     if args.cipher == "caesar":
         res = encryption_caesar(s, key)
     else:
         res = encryption_vigenere(s, key)
 
-if args.act == "decode":
+    write_text(args, res)
+
+
+def process_decode(args):
+    key = args.key
+    if args.cipher == "caesar":
+        key = int(args.key)
+
+    s = get_text(args)
+
     if args.cipher == "caesar":
         res = decryption_caesar(s, key)
     else:
         res = decryption_vigenere(s, key)
 
-if args.act == "train":
+    write_text(args, res)
+
+
+def process_train(args):
     if args.text_file is not None:
-        with open(args.text_file, 'r') as f:
+        with open(args.text_file, "r") as f:
             s = f.read()
     else:
         s = sys.stdin.read()
+
     arr = bar_chart(s)
     json.dump(arr, open(args.model_file, "w"))
 
-if args.act == "hack":
-    with open(args.model_file, 'r') as f:
+
+def process_hack(args):
+    s = get_text(args)
+
+    with open(args.model_file, "r") as f:
         dict = json.load(f)
     key = hack(s, dict)
     res = encryption_caesar(s, key)
 
-if args.act != 'train':
-    if args.output_file is not None:
-        with open(args.output_file, 'w') as f:
-            f.write(res)
-    else:
-        print(res)
+    write_text(args, res)
 
+
+def main():
+    parser = argparse.ArgumentParser()
+
+    subparsers = parser.add_subparsers(help="select mode")
+    encode_parser = subparsers.add_parser("encode")
+    encode_parser.add_argument("--cipher", required=True)
+    encode_parser.add_argument("--key", required=True)
+    encode_parser.add_argument("--input-file", dest="input_file")
+    encode_parser.add_argument("--output-file", dest="output_file")
+    encode_parser.set_defaults(func=process_encode)
+
+    decode_parser = subparsers.add_parser("decode")
+    decode_parser.add_argument("--cipher", required=True)
+    decode_parser.add_argument("--key", required=True)
+    decode_parser.add_argument("--input-file", dest="input_file")
+    decode_parser.add_argument("--output-file", dest="output_file")
+    decode_parser.set_defaults(func=process_decode)
+
+    train_parser = subparsers.add_parser("train")
+    train_parser.add_argument("--text-file")
+    train_parser.add_argument("--model-file", required=True)
+    train_parser.set_defaults(func=process_train)
+
+    hack_parser = subparsers.add_parser("hack")
+    hack_parser.add_argument("--input-file", dest="input_file")
+    hack_parser.add_argument("--output-file", dest="output_file")
+    hack_parser.add_argument("--model-file", required=True)
+    hack_parser.set_defaults(func=process_hack)
+
+    args = parser.parse_args()
+    args.func(args)
+
+
+if __name__ == "__main__":
+    main()
